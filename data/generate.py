bf9f69715590b206301751ccf9a9628ad2efa7f2
Khoa <khoa@Khoas-MBP.top>
2026-01-16 13:29:59 +0700

    Day 2: add circle dataset and visualization

---
+++ data/generate.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def generate_circle_data(n=2000, r=0.5, seed=42):
+    np.random.seed(seed)
+    X = np.random.uniform(-1, 1, size=(n, 2))
+    y = (X[:, 0]**2 + X[:, 1]**2 < r**2).astype(int)
+    return X, y
+
