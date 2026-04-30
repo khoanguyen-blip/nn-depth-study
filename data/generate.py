a9e903f3ced62ea35762f9e28fc4384a1b40c6c0
Khoa <khoa@Khoas-MBP.top>
2026-04-30 13:52:46 +0700

    Day 31: Run depth experiments on distorted rings under identical training conditions
    - Evaluate depths 1,2,4,6,8 with unchanged setup
    - Isolate impact of dataset complexity on optimization behavior

--- data/generate.py
+++ data/generate.py
@@ -21,22 +21,30 @@ def generate_nestedrings_data(n=2000 ,seed=42):
     return X, y.reshape(-1, 1)
 
 
-def generate_distoredrings_data(n= 2000, seed=42): 
+
+
+def generate_distortedrings_data(n=2000, seed=42):
     np.random.seed(seed)
 
-    X = np.random.uniform(-1, 1, size=(n,2)) 
-    x = X[:,0]
-    y = X[:,1] 
+    X = np.random.uniform(-1, 1, size=(n, 2))
+    x = X[:, 0]
+    y = X[:, 1]
 
-    r = np.sqrt(x**2, y**2) 
-    theta = np.arctan2(y,x) 
-    eps = 0.15 
-    k =  5 
-    r_distorted = r + eps*np.sin(k*theta) 
+    # FIX 1
+    r = np.sqrt(x**2 + y**2)
+    theta = np.arctan2(y, x)
 
-    r1 = np.sqrt(2)/3 
-    r2 = 2*np.sqrt(2)/3 
+    eps = 0.15
+    k = 5
+    r_distorted = r + eps * np.sin(k * theta)
 
-    y_label = np.zeros(n, dtype = int ) 
-    mask_middle =  (r_distorted>=r1)&(r_distored<r2) 
-    return X, y_label.reshape(-1,1) 
+    r1 = np.sqrt(2) / 3
+    r2 = 2 * np.sqrt(2) / 3
+
+    y_label = np.zeros(n, dtype=int)
+    mask_middle = (r_distorted >= r1) & (r_distorted < r2)
+
+    # FIX 2
+    y_label[mask_middle] = 1
+
+    return X, y_label.reshape(-1, 1)
